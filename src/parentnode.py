a34e57bdf72c14b613cff4a26c2a33ae53cf1045
Filip Gregor <39672950+fgregor88@users.noreply.github.com>
2026-02-22 21:32:59 +0100

    fix

--- src/parentnode.py
+++ src/parentnode.py
@@ -2,15 +2,18 @@ from htmlnode import HtmlNode
 
 
 class ParentNode(HtmlNode):
-    def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, None, props)
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
-        if self.value is None:
-            raise ValueError("invalid HTML: no value")
         if self.tag is None:
-            return self.value
-        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
     def __repr__(self):
-        return f"ParentNode({self.tag}, {self.value}, {self.props})"
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
