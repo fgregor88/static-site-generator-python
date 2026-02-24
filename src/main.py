add11f30297b42ab666ef1b0d9317eb842be02b4
Filip Gregor <39672950+fgregor88@users.noreply.github.com>
2026-02-24 10:20:31 +0100

    bugfix

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
-from textnode import TextNode, TextType
 from htmlnode import HtmlNode
 from leafnode import LeafNode
+from textnode import TextNode, TextType
+
 
 def main():
-    test = TextNode("test", TextType.PLAIN, "more text")
+    test = TextNode("test", TextType.TEXT, "more text")
     print(test)
 
-    htmlnode = HtmlNode("test", "tets", "test", {"color":  "red", "size": "large"})
+    htmlnode = HtmlNode("test", "tets", "test", {"color": "red", "size": "large"})
     print(htmlnode)
     html = htmlnode.props_to_html()
     print(html)
 
     leafnode = LeafNode("p", "test").to_html()
     print(leafnode)
-
 
 
 if __name__ == "__main__":
