1489a982883c5349bc0b11506b227fc080a0c409
Filip Gregor <39672950+fgregor88@users.noreply.github.com>
2026-03-03 22:11:54 +0100

    block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +10,26 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith("#"):
+        return BlockType.HEADING
+    if block.startswith(">"):
+        return BlockType.QUOTE
+    if block.startswith("```"):
+        return BlockType.CODE
+    if block.startswith("-"):
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1"):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
