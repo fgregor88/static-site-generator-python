dc98db7170d797196d6b744baac140c90c8c35c5
Filip Gregor <39672950+fgregor88@users.noreply.github.com>
2026-03-06 20:35:47 +0100

    copy files

--- src/main.py
+++ src/main.py
@@ -1,19 +1,37 @@
-from htmlnode import HtmlNode
-from leafnode import LeafNode
-from textnode import TextNode, TextType
+from os import listdir, mkdir
+from os.path import exists, isdir, isfile, join
+from shutil import copy, rmtree
 
 
 def main():
-    test = TextNode("test", TextType.TEXT, "more text")
-    print(test)
+    path = "static/"
+    target = "public/"
+    clean_dir(target)
+    inspect_dir(path, target)
 
-    htmlnode = HtmlNode("test", "tets", "test", {"color": "red", "size": "large"})
-    print(htmlnode)
-    html = htmlnode.props_to_html()
-    print(html)
 
-    leafnode = LeafNode("p", "test").to_html()
-    print(leafnode)
+def clean_dir(target_path: str):
+    if exists(target_path):
+        rmtree(target_path)
+    mkdir(target_path)
+
+
+def inspect_dir(src_path: str, target_path: str):
+    paths = listdir(src_path)
+    for path in paths:
+        full_path = join(src_path, path)
+        if isfile(full_path):
+            target = join(target_path, full_path.replace("static/", ""))
+            print(f"{path} | is a file | located at: {full_path} | copy to {target}")
+            copy(full_path, target.replace(f"{path}", ""))
+        elif isdir(full_path):
+            target = join(target_path, full_path.replace("static/", ""))
+            print(f"{path} | is a dir | located at: {full_path} | copy to {target}")
+            mkdir(target)
+            print(listdir("public/"))
+            inspect_dir(full_path, target_path)
+        else:
+            print("fuck")
 
 
 if __name__ == "__main__":
